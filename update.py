0ed673b3b8e20464a0746b593ef990d1db487aa0
Amitash <amitu895@gmail.com>
2021-11-11 21:50:01 +0530

    Added code to authenticate with the nodejs server to obtain credentials for MySQL DB

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import sys, mysql.connector
+import sys, mysql.connector, requests
 
 if len(sys.argv) != 2:
   sys.exit("Run with an argument as: ./update.py <token>")
@@ -8,7 +8,27 @@ if len(sys.argv) != 2:
 token = sys.argv[1] #the first argument is the file itself
 #the second argument is the token entered to authenticate the developer with the server
 
-#code to authenticate with token
+url = 'https://unamused-shrew-2596.dataplicity.io/auth'
+headers = {'Content-Type' : 'application/json'}
+token_json = {'token' : token}
+
+res = requests.post(url, headers = headers, json = token_json)
+
+if res.status_code != 200:
+	sys.exit(res.text)
+
+print('Successfully authenticated with token...')
+
+cred = eval(res.text)
+
+db = mysql.connector.connect(
+  host = cred.get('host'),
+  user = cred.get('user'),
+  password = cred.get('pass'),
+  database = cred.get('db')
+)
+
+print('Successfully authenticated with the database server...')
 
 cur = db.cursor()
 
