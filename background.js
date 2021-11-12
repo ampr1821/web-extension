dcc3a6ba93a6cebe738f15c5712e4d49e1b2a302
Amitash <amitu895@gmail.com>
2021-11-12 19:14:21 +0530

    URL is sent completely but one tick behind

--- background.js
+++ background.js
@@ -1,19 +1,24 @@
 // background.js
 
 // const Url = 'https://unamused-shrew-2596.dataplicity.io/';
+url = "fakeurl"
 
+function update(url_) {
+  url = url_;
+}
 
 chrome.tabs.onUpdated.addListener(()=>{
   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-    url = tabs[0].url;
-    fetch('http://localhost:9000/query',
-    {
-      method : 'POST',
-      cache : 'no-cache',
-      headers : {'Content-Type' : 'application/json'},
-      body : JSON.stringify({"domain" : url})
-    }).then(function(response) {
-      console.log(response);
-    })
+    update(tabs[0].url);
+    alert('hi');
   });
+  fetch('http://localhost:9000/query',
+  {
+    method : 'POST',
+    cache : 'no-cache',
+    headers : {'Content-Type' : 'application/json'},
+    body : JSON.stringify({"domain" : url})
+  }).then(function(response) {
+    console.log(response);
+  })
 });
