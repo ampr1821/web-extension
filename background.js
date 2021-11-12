3e2f12bd00b79bc08fa63a2271670c9898d28486
Amitash <amitu895@gmail.com>
2021-11-12 19:01:50 +0530

    Working - can get url partially

--- background.js
+++ background.js
@@ -2,14 +2,18 @@
 
 // const Url = 'https://unamused-shrew-2596.dataplicity.io/';
 
+
 chrome.tabs.onUpdated.addListener(()=>{
-  fetch('http://localhost:9000/query',
-  {
-    method : 'POST',
-    cache : 'no-cache',
-    headers : {'Content-Type' : 'application/json'},
-    body : JSON.stringify({"token" : "b15621f9df4cf25f"})
-  }).then(function(response) {
-    console.log(response);
-  })
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    url = tabs[0].url;
+    fetch('http://localhost:9000/query',
+    {
+      method : 'POST',
+      cache : 'no-cache',
+      headers : {'Content-Type' : 'application/json'},
+      body : JSON.stringify({"domain" : url})
+    }).then(function(response) {
+      console.log(response);
+    })
+  });
 });
